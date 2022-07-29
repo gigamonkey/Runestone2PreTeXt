f7033fbb4b48fd599f241daa23d160fd04da5dab
Kristi Nakaj <krist@Kristis-MacBook-Pro.local>
2022-07-29 15:25:17 -0400

    Fixed the script based on the suggestions

--- index2main.py
+++ index2main.py
@@ -1,37 +1,29 @@
-
 import os
-import sys
 import re
 from pathlib import Path
 
 id = os.path.basename(os.getcwd())
 permid = id[0:3]
 
-def convert_index2main(root, path):
+def convert_index2main(path):
     tt = open(path).readlines()
-    newroot = root.replace("_sources", "pretext")
+    newroot = "./pretext"
     with open(os.path.join(newroot, "main.ptx"), "w") as ttx:
         ttx.write(
             """\
 <?xml version="1.0" encoding="UTF-8"?>\n"""
         )
-        ttx.write(
-            '    <pretext xmlns:xi="http://www.w3.org/2001/XInclude" xml:lang="en-US">\n'
+        ttx.write(f'''    <pretext xmlns:xi="http://www.w3.org/2001/XInclude" xml:lang="en-US">
+        <xi:include href="./bookinfo.ptx"/>
+        <book xml:id= "{id}" permid="{permid}">\n'''
         )
-        ttx.write(
-            '   <xi:include href="./bookinfo.ptx"/> \n '
-        )
-        ttx.write(
-            '   <book xml:id= "'f'{id}''" permid="'f'{permid}''"> \n '
-        )
-
         booktitle = (tt.index(list(filter(lambda a: '===' in a, tt))[0])) + 1
         ttx.write(
-            f"  <title>{tt[booktitle].strip()}</title>\n"
+                f"            <title>{tt[booktitle].strip()}</title>\n"
         )
 
         ttx.write(
-            '   <subtitle>The PreTeXt Interactive Edition</subtitle> \n'
+                '            <subtitle>The PreTeXt Interactive Edition</subtitle> \n'
         )
 
         for istart, line in enumerate(tt):
@@ -45,24 +37,15 @@ def convert_index2main(root, path):
             if ".rst" in line:
                 fname = line.replace(".rst", "").strip()
                 if fname:
-                    ttx.write(f'        <xi:include href="./{fname}.ptx" />\n')
+                    ttx.write(f'                <xi:include href="./{fname}.ptx" />\n')
+        ttx.write('''            <backmatter>
+                <index>
+                    <index-list/>
+                </index>
+            </backmatter>
+        </book>
+    </pretext>''')
 
 
-        ttx.write("    <backmatter>\n")
-        ttx.write("    <index>\n")
-        ttx.write("    <index-list/>\n")
-        ttx.write("    </index>\n")
-        ttx.write("    </backmatter>\n")
-        ttx.write("    </book>\n")
-        ttx.write("    </pretext>\n")
-
-
-
-# sys.argv[1] should be path to the top level of the book
-os.chdir(sys.argv[1])
-
-# Recursively walk the tree
-for root, dirs, files in os.walk("."):
-    for file in files:
-        if file == "index.rst":
-            convert_index2main(root, Path(os.path.join(root, file)))
+file = "./_sources/index.rst"
+convert_index2main(Path(os.path.join(".", file)))
