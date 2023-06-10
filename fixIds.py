c6987af41556ec4cc5dd16ce6fe2b13a124cd8b4
Peter Seibel <peter@gigamonkeys.com>
2023-06-10 13:21:43 -0700

    More systematic uniqing of ids.

--- fixIds.py
+++ fixIds.py
@@ -1,21 +1,33 @@
 import re
 import os
 
-counter = 0
-idval = 1
+seen = set()
+
+def rewrite_id(m):
+    text = re.sub(r'\s+', "-", m.group(1))
+    text = re.sub(r'^([^A-Za-z_])', r'_\1', text)
+    text = uniquify(text or "empty")
+    return f'xml:id="{text}"'
+
+
+def uniquify(text):
+    orig = text
+    counter = 1
+    while text in seen:
+        text = f"{orig}-{counter}" 
+        counter += 1
+    seen.add(text)
+    return text
+
+
 for root, dirs, files in os.walk("pretext"):
     for file in files:
-        counter += 1
         if "toctree" in file:
             continue
         if ".ptx" in file:
             with open(os.path.join(root, file)) as f:
                 text = f.read()
-            text = text.replace('"id1 ', f'"_{counter}_id1')
-            text = text.replace('"id2 ', f'"_{counter}_id2')
-            text = text.replace(" index-0", f"{counter}_index-0")
-            text = text.replace(" index-1", f"{counter}_index-1")
-            text = re.sub(r'xml:id="([\w+-]+) ([\w+-]+)"', r'xml:id="\1"', text)
+            text = re.sub(r'xml:id="(.*?)"', rewrite_id, text)
 
             with open(os.path.join(root, file), "w") as f:
                 f.write(text)
